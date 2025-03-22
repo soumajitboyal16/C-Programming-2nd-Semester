83b2c284a30cb716bc81b60802aa7ad183f230f8
Soumajit Boyal <soumajitboyal@gmail.com>
2025-03-22 11:37:09 +0530

    Add files via upload

---
+++ Day 10/Matrix Addition.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d",&r,&c);
+    int A[r][c],B[r][c],sum[r][c];
+    printf("Enter elements of first matrix:\n");
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&A[i][j]);
+    printf("Enter elements of second matrix:\n");
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&B[i][j]);
+    printf("Sum of matrices:\n");
+    for(i=0;i<r;i++) 
+	{
+        for(j=0;j<c;j++) 
+		{
+            sum[i][j]=A[i][j]+B[i][j];
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
